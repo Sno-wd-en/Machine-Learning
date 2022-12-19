2ef63b832b6ca26c575b507b585a5c764e49d63b
Hardik <varshney.hardik@gmail.com>
2022-12-19 14:27:55 +0530

    Gradient Descent with 4 vectors and 5 vectors gives pretty good fit

--- Gradient Descent.py
+++ Gradient Descent.py
@@ -2,51 +2,38 @@
 import csv
 import random, math, numpy as np
 from matplotlib import pyplot as plt
-data_set = open("Salary_Data.csv")
+data_set = open("basketball.csv")
 data = csv.reader(data_set)
-x = []
+x=[]
 y=[]
 next(data)
 for k in data:
-    x.append(float(k[0]))
-    y.append(float(k[1]))
+    x.append([1,float(k[0]),float(k[1]),float(k[2]),float(k[3])])
+    y.append([float(k[4])])
 data_set.close()
+x = np.array(x)
+y = np.array(y)
 def gd(x,y):
     min = float('inf')
     thet = 0
-    for epoch in range (1,10):
-        while True:
-            r = int(random.random()*(len(x)-1))
-            s = int(random.random()*(len(x)-1))
-            try:
-                theta = np.array([(y[r]*x[s]-x[r]*y[s])/(x[s]-x[r]),(y[r]-y[s])/(x[r]-x[s])])
-                break
-            except ZeroDivisionError:
-                pass
-        for t in range(1,100):
-            err = 0
-            e = 0
-            alpha = 0.004*math.exp(-t)
-            for k in range (len(x)):
-                h = theta[0]+x[k]*theta[1]
-                h = theta[0]+x[k]*theta[1]
-
-                err += ((y[k] - h)*x[k])
-                e += 0.5*((h-y[k])**2)
-            theta = theta + alpha*err
-            if e < min:
-                thet = theta
-                min = e
-    return (min,thet)
+    minepoch = 1
+    theta = np.array([[1,1,1,1,1]])
+    for epoch in range (1,10000):
+        h=0
+        err = y - np.matmul(x,np.transpose(theta))
+        for k in range (len(x)):
+            h += err[k][0]*x[k]
+        cost = np.sum((err)**2)
+        alpha = 0.828134/1000000*math.exp(-epoch/25)
+        theta = theta + alpha*h
+        if cost < min:
+            thet = theta
+            min = cost
+            minepoch = epoch
+    return (min,thet,minepoch)
 theta = gd(x,y)
 print (theta)
-y1 = []
-for i in x:
-    y1.append(theta[1][0]+theta[1][1]*i)
-plt.plot(x,y)
-plt.plot(x, y1)
-# plt.plot(x,y2)
-plt.show()
+    
 
 
 
